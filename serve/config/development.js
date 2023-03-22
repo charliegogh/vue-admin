d5f477b2faada00f1323622314797fbc5a8683e5
charlie <charliegogh@163.com>
2023-03-22 16:40:18 +0800

    ~~~

---
+++ serve/config/development.js
@@ -0,0 +1,15 @@
+module.exports = {
+  port: 3000, // 服务器端口
+  sequelize: {
+    username: 'root',
+    password: 'xxx',
+    database: 'xxx',
+    host: 'xxx',
+    dialect: 'xxx',
+    define: {
+      underscored: false,
+      paranoid: true
+    }
+  },
+  token: 'charlie'
+}
