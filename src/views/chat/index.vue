0796fc3d61ce723eb6e32cedade734bd240d0546
DESKTOP-0M7NIR5\charlie <charliegogh@gmail.com>
2023-05-11 19:55:56 +0800

    no message

--- src/views/chat/index.vue
+++ src/views/chat/index.vue
@@ -1,6 +1,78 @@
 <template>
-  <a-card :bordered="false">111</a-card>
+  <a-card :bordered="false">
+    <div class="pdf-container">
+      <div
+        id="viewer"
+        ref="attachRef"
+        class="PdfHighlighter"
+      >
+        <div class="pdfViewer" />
+      </div>
+    </div>
+  </a-card>
 </template>
 <script>
-export default {};
+import { getDocument, GlobalWorkerOptions } from 'pdfjs-dist/legacy/build/pdf'
+import pdfjsWorker from 'pdfjs-dist/build/pdf.worker.entry'
+GlobalWorkerOptions.workerSrc = pdfjsWorker
+import {
+  EventBus,
+  PDFViewer,
+  PDFLinkService
+} from 'pdfjs-dist/legacy/web/pdf_viewer'
+import 'pdfjs-dist/web/pdf_viewer.css'
+export default {
+  data() {
+    return {
+      containerNode: null
+    }
+  },
+  mounted() {
+    this.load()
+  },
+  methods: {
+    async load() {
+      var loadingTask = getDocument('./1.pdf')
+      let viewer = document.getElementById('viewer');
+      loadingTask.promise.then(
+        pdf => {
+          const pdfViewer = new PDFViewer({
+            container: viewer,
+            textLayerMode: 2,
+            annotationMode: 2,
+            annotationEditorMode: 0,
+            enablePrintAutoRotate: true,
+            useOnlyCssZoom: false,
+            maxCanvasPixels: 16777216,
+            enablePermissions: false,
+            removePageBorders: true,
+            linkService: self.linkService,
+            renderer: 'canvas',
+            l10n: null,
+          })
+          console.log(pdfViewer)
+          pdfViewer.setDocument(pdf)
+        },
+        reason => {
+          // PDF loading error
+          console.error(reason)
+        }
+      )
+    }
+  }
+}
 </script>
+<style lang="less">
+.pdf-container{
+  position: relative;
+  width: 100%;
+  height: calc(100vh - 75px);
+  .PdfHighlighter{
+    position: absolute;
+    overflow: auto;
+    width: 100%;
+    height: calc(100vh - 75px);
+  }
+}
+
+</style>
