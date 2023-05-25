577ea15fcf6df0a978340e346163802b8af4a052
DESKTOP-N1JBP5A\charlie <charliegogh@gmail.com>
2023-05-25 09:07:53 +0800

    111

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,8 @@
 <script>
 import zhCN from 'ant-design-vue/lib/locale-provider/zh_CN'
 import ResizeHandler from './mixins/ResizeHandler'
+
+console.log(2)
 export default {
   name: 'App',
   mixins: [ResizeHandler],
