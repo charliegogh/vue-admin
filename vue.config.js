bbb4d2964ba3f5193a2a7792ddea3e0a601a33ff
DESKTOP-N1JBP5A\charlie <charliegogh@gmail.com>
2023-06-08 11:28:24 +0800

    no message

--- vue.config.js
+++ vue.config.js
@@ -62,7 +62,7 @@ module.exports = {
     // 是否使用 css 分离插件 ExtractTextPlugin，采用独立样式文件载入，不采用 <style> 方式内联至 html 文件中
     // extract: true,
     // 是否构建样式地图，false 将提高构建速度
-    sourceMap: false, // 关闭 否则热更新失效
+    sourceMap: false, // 关闭 否则热更新失效11
     loaderOptions: {
       less: {
         javascriptEnabled: true
